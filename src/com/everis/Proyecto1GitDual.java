f53c51c1ae79648aca7d42906bd25c2e6bb88755
frodmori <franciscodepaula.rodriguezmorillo.st@everis.nttdata.com>
2022-03-29 14:46:24 +0200

    saludo cambiado

--- src/com/everis/Proyecto1GitDual.java
+++ src/com/everis/Proyecto1GitDual.java
@@ -5,7 +5,7 @@ public class Proyecto1GitDual {
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
-		System.out.println("Hola Mundo!");
+		System.out.println("Hola soy Francisco Rodriguez alumno de practicas en nttdata!");
 
 	}
 
