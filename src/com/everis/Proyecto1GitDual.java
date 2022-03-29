64bbf89468b3224bb1ca9e2898bd8031e0c7c07a
frodmori <franciscodepaula.rodriguezmorillo.st@everis.nttdata.com>
2022-03-29 14:44:51 +0200

    inicial

---
+++ src/com/everis/Proyecto1GitDual.java
@@ -0,0 +1,12 @@
+package com.everis;
+
+public class Proyecto1GitDual {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo!");
+
+	}
+
+}
